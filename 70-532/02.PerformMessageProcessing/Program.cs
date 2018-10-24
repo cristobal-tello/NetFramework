7b8e9e2cc180ac744a35cec4dfc1ef1509e49849
Cristóbal Tello <cristobal.tello@outlook.com>
2018-10-24 12:03:22 +0200

    Initial commit for 02 project

--- 70-532/02.PerformMessageProcessing/Program.cs
+++ 70-532/02.PerformMessageProcessing/Program.cs
@@ -1,15 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System;
 
 namespace _02.PerformMessageProcessing
 {
     class Program
     {
+        // In this example, we don't use Connect Service
+        // Instead we use nuGet. We add: WindowsAzure.Storage, Microsoft.WindowsAzure.ConfigurationManager
         static void Main(string[] args)
         {
+            var storageAccountConnectionString = CloudConfigurationManager.GetSetting("StorageConnectionString");
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageAccountConnectionString);
+
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            CloudQueue queue = queueClient.GetQueueReference("myqueue");
+
+            queue.CreateIfNotExists();
+
+            CloudQueueMessage message = new CloudQueueMessage("Add to queue 9!!");
+            queue.AddMessage(message);
+
+            CloudQueueMessage peekMessage = queue.PeekMessage();
+
+            Console.WriteLine(peekMessage.AsString);
+
+            Console.WriteLine("Looping");
+            foreach (CloudQueueMessage msg in queue.GetMessages(5, TimeSpan.FromMinutes(5)))
+            {
+                Console.WriteLine(msg.AsString);
+            }
+
+            Console.WriteLine("Finish");
+            Console.ReadLine();
         }
     }
 }
